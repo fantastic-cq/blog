14cedba485b48233760e955c8d2c32140703cb70
游德梁 <youdeliang@youdeliangdeMacBook-Pro.local>
2021-12-09 12:31:30 +0800

    fix config

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,11 @@ set -e
 # 构建
 npm run build
 # 导航到构建输出目录
-cd docs/.vuepress/dist
+cd src/.vuepress/dist
 
 git init
 git add -A
 git commit -m 'deploy'
 
 # 推到你仓库的 master 分支
-git push -f https://github.com/fantastic-cq/blog master
+git push -f git@github.com:fantastic-cq/blog.git master
